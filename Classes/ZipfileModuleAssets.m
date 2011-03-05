c3a37f2a1ff890e4fcf269695750f97add48f95f
Kosso <kosso1@gmail.com>
2011-03-05 21:35:35 +0000

    adding new folders

---
+++ Classes/ZipfileModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ZipfileModuleAssets.h"
+
+extern NSData * dataWithHexString (NSString * hexString);
+
+@implementation ZipfileModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
