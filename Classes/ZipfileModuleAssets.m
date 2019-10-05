1cc988f94fdd681a03107efa8ab53a3711966709
hansemannn <hans.knoechel@hs-osnabrueck.de>
2019-10-05 22:56:31 +0200

    chore: add generated meta files

--- Classes/ZipfileModuleAssets.m
+++ Classes/ZipfileModuleAssets.m
@@ -3,13 +3,22 @@
  */
 #import "ZipfileModuleAssets.h"
 
-extern NSData * dataWithHexString (NSString * hexString);
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
 
 @implementation ZipfileModuleAssets
 
-- (NSData*) moduleAsset
+- (NSData *)moduleAsset
 {
-	return nil;
+  
+
+  return nil;
+}
+
+- (NSData *)resolveModuleAsset:(NSString *)path
+{
+  
+
+  return nil;
 }
 
 @end
