e5767bd3cf56ab82ee520145638e2e1102b46079
hansemannn <hans.knoechel@hs-osnabrueck.de>
2019-10-05 22:55:20 +0200

    chore: remove unused lifecycle methods

--- Classes/ZipFileModule.m
+++ Classes/ZipFileModule.m
@@ -25,57 +25,6 @@
 	return @"zipfile";
 }
 
-#pragma mark Lifecycle
-
--(void)startup
-{
-	// this method is called when the module is first loaded
-	// you *must* call the superclass
-	[super startup];
-	
-	NSLog(@"[INFO] %@ loaded",self);
-}
-
--(void)shutdown:(id)sender
-{
-	// this method is called when the module is being unloaded
-	// typically this is during shutdown. make sure you don't do too
-	// much processing here or the app will be quit forcibly
-	
-	// you *must* call the superclass
-	[super shutdown:sender];
-}
-
-#pragma mark Internal Memory Management
-
--(void)didReceiveMemoryWarning:(NSNotification*)notification
-{
-	// optionally release any resources that can be dynamically
-	// reloaded once memory is available - such as caches
-	[super didReceiveMemoryWarning:notification];
-}
-
-#pragma mark Listener Notifications
-
--(void)_listenerAdded:(NSString *)type count:(int)count
-{
-	//if (count == 1 && [type isEqualToString:@"my_event"])
-	//{
-		// the first (of potentially many) listener is being added 
-		// for event named 'my_event'
-	//}
-}
-
--(void)_listenerRemoved:(NSString *)type count:(int)count
-{
-	//if (count == 0 && [type isEqualToString:@"my_event"])
-	//{
-		// the last listener called for event named 'my_event' has
-		// been removed, we can optionally clean up any resources
-		// since no body is listening at this point for that event
-	//}
-}
-
 #pragma Public APIs
 
 -(void)extract:(id)args
